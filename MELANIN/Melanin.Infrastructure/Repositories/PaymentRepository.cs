739c062732feedf03aae4b8f7305087d5e96adb7
Lopez-Vie <mr.jasy@live.be>
2026-05-19 04:57:55 +0200

    feat(payment): add IPaymentRepository and PaymentRepository

---
+++ MELANIN/Melanin.Infrastructure/Repositories/PaymentRepository.cs
@@ -0,0 +1,36 @@
+﻿using Melanin.Application.Interfaces.Repositories;
+using Melanin.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Melanin.Infrastructure.Database.Repositories;
+
+public class PaymentRepository : IPaymentRepository
+{
+    private readonly MelaninDbContext _dbContext;
+
+    public PaymentRepository(MelaninDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<Payment> AddAsync(Payment payment)
+    {
+        await _dbContext.Payments.AddAsync(payment);
+        await _dbContext.SaveChangesAsync();
+
+        return payment;
+    }
+
+    public async Task<Payment?> GetByStripeSessionIdAsync(string stripeSessionId)
+    {
+        return await _dbContext.Payments
+            .Include(p => p.Order)
+            .FirstOrDefaultAsync(p => p.StripeSessionId == stripeSessionId);
+    }
+
+    public async Task UpdateAsync(Payment payment)
+    {
+        _dbContext.Payments.Update(payment);
+        await _dbContext.SaveChangesAsync();
+    }
+}
