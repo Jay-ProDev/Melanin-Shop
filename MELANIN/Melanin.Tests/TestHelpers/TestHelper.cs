7bdceb71535e3e123abb8cc2838a73eed3ee0d16
Jay-ProDev <jay.pro.dev@gmail.com>
2026-07-07 20:04:04 +0200

    test: add SetProperty method to test helper for private properties

--- MELANIN/Melanin.Tests/TestHelpers/TestHelper.cs
+++ MELANIN/Melanin.Tests/TestHelpers/TestHelper.cs
@@ -4,11 +4,18 @@ namespace Melanin.Tests.TestHelpers
 {
     public static class TestHelper
     {
-        // Force la propriété "Id" (private set) d'une entité, uniquement pour les tests
+        // Force la propriété "Id" d'une entité (private set géré par EF Core)
         public static void SetId(object entity, int id)
         {
             PropertyInfo? property = entity.GetType().GetProperty("Id");
             property!.SetValue(entity, id);
+        }
+
+        // Force n'importe quelle propriété privée d'une entité (pour les tests)
+        public static void SetProperty(object entity, string propertyName, object? value)
+        {
+            PropertyInfo? property = entity.GetType().GetProperty(propertyName);
+            property!.SetValue(entity, value);
         }
     }
 }
